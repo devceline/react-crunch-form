26a13258c53b5412370a4ebcc853897f865fecc1
Shaheen Sarafa <contact@devshaheen.com>
2021-03-13 20:25:42 +0200

    Making sample

---
+++ sample/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {CrunchForm} from "react-crunch-form";
+
+function App() {
+  return (
+    <div className="App">
+      <CrunchForm className="test">
+        <span>Hello</span>
+      </CrunchForm>
+    </div>
+  );
+}
+
+export default App;
